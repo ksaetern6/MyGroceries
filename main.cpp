b6853dbf139b294420f7b4cdfc56896e71bee5d3
ksaetern6 <ksaetern6@mail.csuchico.edu>
2019-02-13 11:46:48 -0800

    Added loop for list and input

--- main.cpp
+++ main.cpp
@@ -1,19 +1,39 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector<string> list; //array of 5 strings
   int numItems = 0;
   char input;
-
+  
+  do{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+  if(input == 'A'){
+	string name = "";
+	cin>>name;
+	
+	list.push_back(name);
+	numItems++;
+  }//if
+  else if(input == 'Q' || input == 'q'){
+	if(numItems==0){
+		cout << "no items on the list" << endl;
+	}//if
+	else{
+		for(int x = 0; x < list.size(); x++){
+			cout << list[x] << endl;
+		}//for
+	}//else
+  }//else if
+	}while(input != 'Q' && input != 'q');
+  
   return 0;
 }
